668ccd3b691a413320e58c76d5aa3ef69671ebe2
Zach Esposito <zach@zachesposito.me>
2020-03-19 10:58:37 -0400

    progress

---
+++ ranks.js
@@ -0,0 +1,13 @@
+var ranks = [
+  "White",
+  "Yellow",
+  "Gold",
+  "Orange",
+  "Blue",
+  "Green",
+  "Brown",
+  "2nd Brown",
+  "Red",
+  "2nd Red",
+  "Black"
+]
---
+++ techniques.js
@@ -0,0 +1,168 @@
+var techniques = {
+  "blocks":[
+    {
+      "name": "Side step front foot high block front hand",
+      "minimumRank": 0
+    },
+    {
+      "name": "Head hug block",
+      "minimumRank": 0
+    },
+    {
+      "name": "Side step front foot high block back hand",
+      "minimumRank": 1
+    },
+    {
+      "name": "Side step back foot high block front hand",
+      "minimumRank": 1
+    },
+    {
+      "name": "Side step front foot low block front hand",
+      "minimumRank": 2
+    },
+    {
+      "name": "Side step front foot low block back arm",
+      "minimumRank": 2
+    },
+    {
+      "name": "Side step back foot low block front arm",
+      "minimumRank": 2
+    },
+    {
+      "name": "Chudan block",
+      "minimumRank": 4
+    },
+    {
+      "name": "Jodan block",
+      "minimumRank": 4
+    }
+  ],
+  "handStrikes":[
+    {
+      "name": "Jab",
+      "minimumRank": 0
+    },
+    {
+      "name": "Cross",
+      "minimumRank": 0
+    },
+    {
+      "name": "Hook punch",
+      "minimumRank": 0
+    },
+    {
+      "name": "Uppercut",
+      "minimumRank": 0
+    },
+    {
+      "name": "Palm strike",
+      "minimumRank": 1
+    },
+    {
+      "name": "Low palm strike",
+      "minimumRank": 1
+    },
+    {
+      "name": "Nukite strike",
+      "minimumRank": 1
+    },
+    {
+      "name": "Hammer first downward delivery",
+      "minimumRank": 2
+    },
+    {
+      "name": "Hammer first reverse delivery",
+      "minimumRank": 2
+    },
+    {
+      "name": "Elbow strike downward delivery",
+      "minimumRank": 3
+    },
+    {
+      "name": "Elbow strike roundhouse delivery",
+      "minimumRank": 3
+    },
+    {
+      "name": "Elbow strike rising delivery",
+      "minimumRank": 3
+    },
+    {
+      "name": "Elbow strike reverse delivery",
+      "minimumRank": 3
+    },
+    {
+      "name": "Knife hand downward delivery",
+      "minimumRank": 3
+    },
+    {
+      "name": "Knife hand reverse delivery",
+      "minimumRank": 3
+    },
+    {
+      "name": "Heito",
+      "minimumRank": 4
+    },
+    {
+      "name": "Backfist",
+      "minimumRank": 4
+    },
+    {
+      "name": "Snake strike",
+      "minimumRank": 4
+    },
+    {
+      "name": "Crane strike",
+      "minimumRank": 4
+    }
+  ],
+  "legStrikes": [
+    {
+      "name": "Front kick",
+      "minimumRank": 0
+    },
+    {
+      "name": "Knee strike rising delivery",
+      "minimumRank": 0
+    },
+    {
+      "name": "Roundhouse kick",
+      "minimumRank": 1
+    },
+    {
+      "name": "Haku geri",
+      "minimumRank": 2
+    },
+    {
+      "name": "Side kick",
+      "minimumRank": 2
+    },
+    {
+      "name": "Hook kick",
+      "minimumRank": 3
+    },
+    {
+      "name": "Knee strike roundhouse delivery",
+      "minimumRank": 4
+    },
+    {
+      "name": "Inside crescent kick",
+      "minimumRank": 4
+    },
+    {
+      "name": "Outside crescent kick",
+      "minimumRank": 4
+    },
+    {
+      "name": "Axe kick",
+      "minimumRank": 4
+    },
+    {
+      "name": "Fumokomi geri",
+      "minimumRank": 4
+    },
+    {
+      "name": "Back kick",
+      "minimumRank": 4
+    }
+  ]
+}
